dea20f9d7f705bd14b25876a1d279b0ad45868a7
Muhammad Owais Ahmed <owaisraza2051@gmail.com>
2022-03-05 23:03:12 +0500

    Updates

---
+++ precache-manifest.f528c150c0fcadfee6e6ff2adb4f5da2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "760525b90a51abd00ffd",
+    "url": "/static/css/main.e1421262.chunk.css"
+  },
+  {
+    "revision": "760525b90a51abd00ffd",
+    "url": "/static/js/main.760525b9.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1f365f8759caa80da0df3c711a144625",
+    "url": "/index.html"
+  }
+];
