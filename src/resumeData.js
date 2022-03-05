2027a1052aec98e2fbe51e636411a7b8b27da198
Muhammad Owais Ahmed <owaisraza2051@gmail.com>
2022-03-05 23:10:55 +0500

    update

---
+++ src/resumeData.js
@@ -0,0 +1,160 @@
+let resumeData = {
+  "imagebaseurl": "https://codewithowais.github.io/",
+  "name": "Muhammad Owais Ahmed",
+  "role": "Full Stack & Mobile APPs Developer",
+  "linkedinId": "Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " I like to learn about new technologies, write technical articles",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/codewithowais/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/codewithowais",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "http://twitter.com/codewithowais",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am Full Stack WEB and Mobile Apps Developer. and i would like to learn and code on new technologies ",
+  "address": "Karachi, Pakistan",
+  "website": "https://codewithowais.github.io",
+  "education": [
+    {
+      "UniversityName": "Virtual University Of Pakistan",
+      "specialization": "Computer Science",
+      "MonthOfPassing": "",
+      "YearOfPassing": "Progress",
+      "Achievements": ""
+    },
+    {
+      "UniversityName": "Superior Collage",
+      "specialization": "Commerce",
+      "MonthOfPassing": "Jan",
+      "YearOfPassing": "2018",
+      "Achievements": ""
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Dawateislami",
+      "specialization": "Full Stack Developer",
+      "MonthOfLeaving": "Sep 2020 - ",
+      "YearOfLeaving": "Current",
+      "Achievements": ""
+    },
+    {
+      "CompanyName": "Freelance",
+      "specialization": "FULL STACK || MEARN || MEAN || FLUTTER ENGINEER",
+      "MonthOfLeaving": "",
+      "YearOfLeaving": "",
+      "Achievements": ""
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "SCSS"
+    },
+    {
+      "skillname": "Bootstrap"
+    },
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "Jquery"
+    },
+    {
+      "skillname": "TypeScript"
+    },
+    {
+      "skillname": "Dart"
+    },
+    {
+      "skillname": "Flutter"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "Material-UI"
+    },
+    {
+      "skillname": "Redux"
+    },
+    {
+      "skillname": "React Native"
+    },
+    {
+      "skillname": "Angular"
+    },
+    {
+      "skillname": "NodeJS"
+    },
+    {
+      "skillname": "MongoDB"
+    },
+    {
+      "skillname": "Python"
+    },
+    {
+      "skillname": "Django"
+    },
+    {
+      "skillname": "C#"
+    },
+    {
+      "skillname": "dotnet"
+    },
+    {
+      "skillname": "Microsoft SQL Server"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
